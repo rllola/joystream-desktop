215aa4936046e71602fd62fe589659b9223cdc6e
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2017-04-10 14:02:31 +0300

    wallet component - implemented as stateless functional component

--- app/Wallet/Wallet.js
+++ app/Wallet/Wallet.js
@@ -2,26 +2,19 @@ import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
 import QRCode from 'qrcode-react'
 
-@inject('walletStore')
-@observer
-class Wallet extends Component {
-  componentWillMount () {
-
-  }
-
-  render () {
-    if (this.props.walletStore.address == null)
+// Wallet implemented as a functional stateless component
+const Wallet = inject('walletStore')(observer(({walletStore}) => {
+    if (walletStore.address == null)
         return null
 
     return (
       <div style={{marginTop: '20px'}} className="col-10">
         <h3>Wallet</h3>
-        <p>Balance : {this.props.walletStore.balance}</p>
-        <QRCode value={this.props.walletStore.address.toString()} />
-        <p>Address : {this.props.walletStore.address.toString()}</p>
+        <p>Balance : {walletStore.balance}</p>
+        <QRCode value={walletStore.address.toString()} />
+        <p>Address : {walletStore.address.toString()}</p>
       </div>
     )
-  }
-}
+}))
 
 export default Wallet
