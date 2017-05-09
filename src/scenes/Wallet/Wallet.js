061c5e52657e59f1bfd75152f847e30ef4d4e85c
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2017-05-09 10:15:51 +0300

    allow possibility of wallet not being initialized

--- src/scenes/Wallet/Wallet.js
+++ src/scenes/Wallet/Wallet.js
@@ -3,20 +3,21 @@ import { inject, observer } from 'mobx-react'
 import QRCode from 'qrcode-react'
 
 // Wallet implemented as a functional stateless component
-function Wallet(props) {
-    const walletStore = props.walletStore
+function Wallet (props) {
+  const walletStore = props.walletStore
 
-    if (walletStore.address == null)
-        return null
+  if (walletStore == null) return null
 
-    return (
-      <div style={{marginTop: '20px'}} className="col-10">
-        <h3>Wallet</h3>
-        <p>Balance : {walletStore.balance}</p>
-        <QRCode value={walletStore.address.toString()} />
-        <p>Address : {walletStore.address.toString()}</p>
-      </div>
-    )
+  if (walletStore.address == null) return null
+
+  return (
+    <div style={{marginTop: '20px'}} className="col-10">
+      <h3>Wallet</h3>
+      <p>Balance : {walletStore.balance}</p>
+      <QRCode value={walletStore.address.toString()} />
+      <p>Address : {walletStore.address.toString()}</p>
+    </div>
+  )
 }
 
 export default inject('walletStore')(observer(Wallet))
