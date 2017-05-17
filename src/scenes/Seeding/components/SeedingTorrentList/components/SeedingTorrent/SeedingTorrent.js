627fb80deb847bf6539b28b33eeecbed5905ee15
Lola Rigaut-Luczak <rllola80@gmail.com>
2017-05-17 14:12:45 +0200

    Making SeedingTorrent a dumb component

--- src/scenes/Seeding/components/SeedingTorrentList/components/SeedingTorrent/SeedingTorrent.js
+++ src/scenes/Seeding/components/SeedingTorrentList/components/SeedingTorrent/SeedingTorrent.js
@@ -3,44 +3,36 @@ import { inject } from 'mobx-react'
 import { StateT } from 'joystream-node'
 import utils from '../../../../../../utils/'
 
-@inject('applicationStore')
-class SeedingTorrent extends Component {
-
-  constructor (props) {
-    super(props)
-
-    this.startSelling = this.startSelling.bind(this)
-  }
-
-  startSelling () {
-    //  50, 1, 10, 15000, 5000
-    let sellerTerms = {
-      minPrice: 50,
-      minLock: 1,
-      maxNumberOfSellers: 10,
-      minContractFeePerKb: 15000,
-      settlementFee: 5000
-    }
-
-    let infoHash = this.props.torrent.handle.infoHash()
-
-    this.props.applicationStore.sellingTorrent(infoHash, sellerTerms)
-  }
-
-  render () {
-    let torrent = this.props.torrent
-
-    return (
-      <tr>
-        <td>{torrent.name}</td>
-        <td>{torrent.sizeMB} Mb</td>
-        <td>{torrent.progressPercent}%</td>
-        <td>{StateT.properties[torrent.libtorrentState].name}</td>
-        {/* If we have a buyer show button startSelling or startSelling directly after finding it */}
-        <td>{torrent.mode == utils.TorrentMode.SELL_MODE ? <p>In Sell Mode</p> : <button className="btn btn-default" onClick={this.startSelling}>Start selling</button>}</td>
-      </tr>
-    )
-  }
+//  50, 1, 10, 15000, 5000
+// TEMPORARY
+const sellerTerms = {
+  minPrice: 50,
+  minLock: 1,
+  maxNumberOfSellers: 10,
+  minContractFeePerKb: 15000,
+  settlementFee: 5000
 }
+
+const SeedingTorrent = inject('applicationStore')((props) => {
+  const torrent = props.torrent
+  const applicationStore = props.applicationStore
+
+  function startSelling () {
+    let infoHash = torrent.handle.infoHash()
+
+    applicationStore.sellingTorrent(infoHash, sellerTerms)
+  }
+
+  return (
+    <tr>
+      <td>{torrent.name}</td>
+      <td>{torrent.sizeMB} Mb</td>
+      <td>{torrent.progressPercent}%</td>
+      <td>{StateT.properties[torrent.libtorrentState].name}</td>
+      {/* If we have a buyer show button startSelling or startSelling directly after finding it */}
+      <td>{torrent.mode === utils.TorrentMode.SELL_MODE ? <p>In Sell Mode</p> : <button className="btn btn-default" onClick={startSelling}>Start selling</button>}</td>
+    </tr>
+  )
+})
 
 export default SeedingTorrent
