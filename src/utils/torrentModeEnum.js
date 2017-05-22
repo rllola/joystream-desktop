46e257cb28ea7f892ab0f1da9d43943ac2c04319
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2017-05-22 18:59:23 +0300

    add NOT_SET enum

--- src/utils/torrentModeEnum.js
+++ src/utils/torrentModeEnum.js
@@ -1,11 +1,13 @@
 const TorrentMode = {
-  OBSERVE_MODE: 0,
-  SELL_MODE: 1,
-  BUY_MODE: 2,
+  NOT_SET: 0,
+  OBSERVE_MODE: 1,
+  SELL_MODE: 2,
+  BUY_MODE: 3,
   properties: {
-    0: {name: 'Observe Mode', value: 0},
-    1: {name: 'Sell Mode', value: 1},
-    2: {name: 'Buy Mode', value: 2}
+    0: {name: 'Not Set', value: 0},
+    1: {name: 'Observe Mode', value: 1},
+    2: {name: 'Sell Mode', value: 2},
+    3: {name: 'Buy Mode', value: 3}
   }
 }
 
