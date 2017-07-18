f38059b175fbd5d0c474eef02b5dd87523f0be25
Bedeho Mender <bedeho.mender@gmail.com>
2017-07-18 20:04:21 +0200

    finilize basic loading screen component

--- src/component-development/loading/index.js
+++ src/component-development/loading/index.js
@@ -5,13 +5,18 @@
 import React, {Component} from 'react'
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton'
+import Slider from 'material-ui/Slider'
 
 import {ScenarioContainer} from '../common'
 import LoadingScene,{LoadingState} from '../../scenes/Application/LoadingScene'
 
 
 function getState(s) {
-    return { 'loadingState' : LoadingState.CreatingSPVNode }
+
+    return {
+      'loadingState' : s.loadingState,
+      'loadingTorrentsProgressValue': s.loadingTorrentsProgressValue ? s.loadingTorrentsProgressValue : 0
+    }
 }
 
 class ControllableLoadingScene extends Component {
@@ -19,37 +24,77 @@ class ControllableLoadingScene extends Component {
     constructor(props) {
         super(props)
 
-        this.state = getState(LoadingState.CreatingSPVNode)
+        this.state = {
+          loadingState : LoadingState.InitializingResources,
+          loadingTorrentsProgressValue : 18
+        }
+
+        //getState(LoadingState.CreatingSPVNode)
     }
 
     goToNextState() {
 
-        if(this.state.loadingState == LoadingState.Finished)
+        if(!this.state.loadingState == LoadingState.LoadingTorrents)
             throw Error('Already at last state')
         else
-            this.setState(getState(this.state.loadingState + 1)) // a bit sloppy, but OK, later add more sophistication
+            this.setState({loadingState : this.state.loadingState + 1}) // a bit sloppy, but OK, later add more sophistication
     }
 
     goToPriorState() {
 
-        if(this.state.loadingState == LoadingState.CreatingSPVNode)
+        if(this.state.loadingState == LoadingState.InitializingResources)
             throw Error('Already at first state')
         else
-            this.setState(getState(this.state.loadingState - 1)) // a bit sloppy, but OK, later add more sophistication
+            this.setState({loadingState : this.state.loadingState - 1}) // a bit sloppy, but OK, later add more sophistication
+    }
+
+    handleSliderChange = (event, value) => {
+      this.setState({loadingTorrentsProgressValue: 100*value})
     }
 
     render() {
 
+      console.log(this.state)
+
         return (
             <div>
-                <LoadingScene state={this.state.loadingState}/>
+                <div style={{ height : 600}}>
+                  <LoadingScene loadingState={this.state.loadingState} loadingTorrentsProgressValue={this.state.loadingTorrentsProgressValue}/>
+                </div>
                 <CardActions>
-                    <FlatButton label="Next" onTouchTap={() => { this.goToNextState()}}/>
-                    <FlatButton label="Previous" onTouchTap={() => { this.goToPriorState()}}/>
+
+                    {
+                      this.state.loadingState != LoadingState.CreatingSPVNode
+                        ?
+                      <FlatButton label="Previous" onTouchTap={() => { this.goToPriorState()}}/>
+                        :
+                      null
+                    }
+
+                    {
+                      this.state.loadingState != LoadingState.Finished
+                        ?
+                      <FlatButton label="Next" onTouchTap={() => { this.goToNextState()}}/>
+                        :
+                      null
+                    }
+
+                    {
+                      this.state.loadingState == LoadingState.LoadingTorrents
+                        ?
+                      <Slider defaultValue={this.state.loadingTorrentsProgressValue/100} onChange={this.handleSliderChange} />
+                        :
+                      null
+                    }
+
                 </CardActions>
             </div>
         )
     }
+
+}
+
+ControllableLoadingScene.propTypes = {
 
 }
 
@@ -58,7 +103,6 @@ const LoadingSceneScenarios = () => {
     return (
         <ScenarioContainer>
             <ControllableLoadingScene />
-
         </ScenarioContainer>
     )
 }
--- src/scenes/Application/LoadingScene.js
+++ src/scenes/Application/LoadingScene.js
@@ -9,72 +9,75 @@ import {
     Stepper,
     StepLabel,
     StepContent,
-} from 'material-ui/Stepper';
+} from 'material-ui/Stepper'
+import LinearProgress from 'material-ui/LinearProgress'
 
 // Move this into the ?
 // which step can fail?
 // which needs richer state?
 var LoadingState = {
-    CreatingSPVNode : 0,
-    CreateSession : 1,
+    InitializingResources : 0,
+    OpeningApplicationDatabase : 1,
     InitializingSPVNode : 2,
     OpeningWallet : 3,
     ConnectingToBitcoinP2PNetwork : 4,
-    LoadingTorrents : 5,
-    Finished : 6
+    LoadingTorrents : 5
+}
+
+// initializing_resources => (create) dirs, spv, session
+// (open)/initializing_application_database =>
+// initializeSpvNode
+// opening_wallet
+// connecting_to_bitcoin_p2p_network
+
+
+const LoadingProgressIndicator = (props) => {
+
+  return (
+      <Stepper
+      activeStep={props.loadingState}
+      orientation="vertical"
+      connector={null}>
+          <Step>
+              <StepLabel>Initializing resources</StepLabel>
+          </Step>
+          <Step>
+              <StepLabel>Opening application database</StepLabel>
+          </Step>
+          <Step>
+              <StepLabel>Initializing Bitcoin SPV node</StepLabel>
+          </Step>
+          <Step>
+              <StepLabel>Opening wallet</StepLabel>
+          </Step>
+          <Step>
+              <StepLabel>Connecting to Bitcoin peer network</StepLabel>
+          </Step>
+          <Step>
+              <StepLabel>Loading torrents</StepLabel>
+              <StepContent>
+                  <LinearProgress mode="determinate" value={props.loadingTorrentsProgressValue} />
+              </StepContent>
+          </Step>
+      </Stepper>
+  )
 }
 
 const LoadingScene = (props) => {
 
     return (
-        <div style={{maxWidth: 380, maxHeight: 400, margin: 'auto'}}>
-            <Stepper activeStep={props.state} orientation="vertical">
-                <Step>
-                    <StepLabel>Creating SPV Node</StepLabel>
-                </Step>
-                <Step>
-                    <StepLabel>Starting BitTorrent session</StepLabel>
-                </Step>
-                <Step>
-                    <StepLabel>Opening Wallet</StepLabel>
-                </Step>
-                <Step>
-                    <StepLabel>Connecting to Bitcoin peer network</StepLabel>
-                </Step>
-                <Step>
-                    <StepLabel>Loading torrents</StepLabel>
-                    <StepContent>
-                        <p>
-                            Try out different ad text to see what brings in the most customers,
-                            and learn how to enhance your ads using features like ad extensions.
-                            If you run into any problems with your ads, find out how to tell if
-                            they're running and how to resolve approval issues.
-                        </p>
-                    </StepContent>
-                </Step>
-                <Step>
-                    <StepLabel>Finished</StepLabel>
-                </Step>
-            </Stepper>
+      <div className="LoadingScene">
+        <div className="CenterPiece">
+            <h1>logo</h1>
+            <LoadingProgressIndicator {...props}/>
         </div>
+      </div>
     )
-
 }
 
-/*
-<StepContent>
-    <p>
-        Try out different ad text to see what brings in the most customers,
-        and learn how to enhance your ads using features like ad extensions.
-        If you run into any problems with your ads, find out how to tell if
-        they're running and how to resolve approval issues.
-    </p>
-</StepContent>
-*/
-
 LoadingScene.propTypes = {
-    state : PropTypes.oneOf(Object.values(LoadingState))
-
+    loadingState : PropTypes.oneOf(Object.values(LoadingState)),
+    loadingTorrentsProgressValue : PropTypes.number
 }
 
 export {LoadingState}
