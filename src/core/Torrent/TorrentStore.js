1545ed2a9a95e3b7cd4dbcb6ecdb0cdb50d68fcd
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2017-07-25 15:18:34 +0300

    fixed computed decorator

--- src/core/Torrent/TorrentStore.js
+++ src/core/Torrent/TorrentStore.js
@@ -187,13 +187,13 @@ class TorrentStore {
 
     }
 
-    @computer get canStop() {
+    @computed get canStop() {
         return this.state.startsWith("Active.DownloadIncomplete.Unpaid.Started.ReadyForStartPaidDownloadAttempt") ||
             this.state.startsWith("Active.FinishedDownloading.Uploading.Started")
 
     }
 
-    @compute get canStart() {
+    @computed get canStart() {
         return this.state.startsWith("Active.DownloadIncomplete.Unpaid.Stopped") ||
             this.state.startsWith("Active.FinishedDownloading.Uploading.Stopped")
     }
