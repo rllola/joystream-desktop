ba67b7ae21c9185092aa69556759db61ec0b8150
rllola <rllola80@gmail.com>
2017-07-27 13:42:12 +0200

    Refactoring

--- src/components/StreamFile/File.js
+++ src/components/StreamFile/File.js
@@ -1,48 +1,25 @@
-import FileStream from './FileStream'
-import { EventEmitter } from 'events'
+import LibtorrentStream from './LibtorrentStream'
 
-class File extends EventEmitter {
+class File {
     constructor (torrent, fileIndex) {
       super()
 
       this._torrent = torrent
-      this._destroyed = false
 
       var torrentInfo = torrent.handle.torrentFile()
-      var fileStorage = torrentInfo.files()
 
-      this.pieceLength = torrentInfo.pieceLength()
+      // Require by media-render
       this.name = fileStorage.fileName(fileIndex)
-      this.path = fileStorage.filePath(fileIndex)
-      this.size = fileStorage.fileSize(fileIndex)
-      this.offset = fileStorage.fileOffset(fileIndex)
-
-      this.done = false
-
-      var start = this.offset
-      var end = start + this.size
-
-      this._startPiece = start / this.pieceLength | 0
-      this._endPiece = end / this.pieceLength | 0
-
-      if (this.size === 0) {
-        this.done = true
-        this.emit('done')
-      }
     }
 
-    createReadStream (opts) {
-      var self = this
-      if (!opts) opts = {}
-
-      var fileStream = new FileStream(self, opts)
+    /**
+     * Called by render-media. Will return the ReadableStream object.
+     * @param {object} opts : {start: bytes, end: bytes}
+     */
+    createReadStream (opts = {}) {
+      var fileStream = new LibtorrentStream(torrent, opts)
 
       return fileStream
-    }
-
-    _destroy () {
-      this._destroyed = true
-      this._torrent = null
     }
 }
 
