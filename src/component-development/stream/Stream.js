e5425faad3554af1e3157907eba8ede7b1e03ee8
rllola <rllola80@gmail.com>
2017-08-14 09:36:30 +0200

    WIP

--- src/component-development/stream/Stream.js
+++ src/component-development/stream/Stream.js
@@ -10,7 +10,7 @@ const StreamScenario = () => {
         <div>
             <ScenarioContainer title="Stream" subtitle="stream sintel video">
             <video width="100%" height="auto" autoPlay controls>
-              <source src="https://download.blender.org/durian/movies/sintel_4k.mov" />
+              <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
             </video>
             </ScenarioContainer>
         </div>
