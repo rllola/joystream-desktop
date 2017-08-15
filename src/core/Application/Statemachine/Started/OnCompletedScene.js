846f9db821bf00f2da2377f98ce457525252da50
Bedeho Mender <bedeho.mender@gmail.com>
2017-08-15 08:04:34 +0200

    set number completed in background when navigating to completed scene

--- src/core/Application/Statemachine/Started/OnCompletedScene.js
+++ src/core/Application/Statemachine/Started/OnCompletedScene.js
@@ -8,9 +8,18 @@ var OnCompletedScene = new BaseMachine({
   initialState: 'idle',
   states: {
     idle: {
+
+      _onEnter : function (client) {
+
+        // Since we are going to the Completed scene,
+        // we mark all as no longer completed in the background
+        client.store.setNumberCompletedInBackground(0)
+      },
+
       downloading_scene_selected: function (client) {
         this.go(client, '../OnDownloadingScene')
       },
+
       uploading_scene_selected: function (client) {
         this.go(client, '../OnUploadingScene')
       }
