49709c5ed238da11dd2dc4365e8888bf085a8097
rllola <rllola80@gmail.com>
2017-08-16 19:00:50 +0200

    Created video component UI

--- src/component-development/stream/Stream.js
+++ src/component-development/stream/Stream.js
@@ -4,17 +4,86 @@ import {ScenarioContainer} from '../common'
 import os from 'os'
 import path from 'path'
 
-const StreamScenario = () => {
+class StreamScenario extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      videoPlayer: false
+    }
+  }
+
+  handleClick (event) {
+    event.preventDefault()
+
+    this.setState({videoPlayer: true})
+  }
+
+  handleClose (event) {
+    event.preventDefault()
+
+    this.setState({videoPlayer: false})
+  }
+
+  render () {
+    const blockStyle = {
+      position: 'relative',
+      width: '100%',
+      height: '100%'
+    }
 
     return (
-        <div>
-            <ScenarioContainer title="Stream" subtitle="stream sintel video">
-            <video width="100%" height="auto" autoPlay controls>
-              <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-            </video>
-            </ScenarioContainer>
-        </div>
+      <div>
+          <ScenarioContainer title="Stream" subtitle="stream sintel video">
+            <div style={blockStyle} >
+              <h1>Some scenes here</h1>
+              <br/>
+              <br/>
+              <button onClick={this.handleClick.bind(this)}>Play</button>
+              {this.state.videoPlayer ? <VideoPlayer closeVideoPlayer={this.handleClose.bind(this)} /> : null}
+              <br/>
+              <br/>
+            </div>
+          </ScenarioContainer>
+      </div>
     )
+  }
+}
+
+const VideoPlayer = (props) => {
+  const overlayStyle = {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: 0,
+    background: 'black'
+  }
+
+  return (
+    <div>
+      <CloseButton closeVideoPlayer={props.closeVideoPlayer} />
+      <div style={overlayStyle}>
+        <video width="100%" height="auto" controls>
+          <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+        </video>
+      </div>
+    </div>
+  )
+}
+
+const CloseButton = (props) => {
+
+  const closeStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 5,
+    color: 'white',
+    zIndex: 99
+  }
+
+  return (
+    <a style={closeStyle} onClick={props.closeVideoPlayer} href="#"><span >X</span></a>
+  )
 }
 
 export default StreamScenario
