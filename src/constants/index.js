2354a62882e8d49e5e9d49781c393cbd41f32ebe
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2017-09-07 16:20:14 +0300

    fix constants exports

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
 import path from 'path'
 import os from 'os'
 
-export default {
-  POST_TORRENT_UPDATES_INTERVAL: 3000,
-  DEFAULT_TORRENT_FILE_SOURCE_LOCATION: os.homedir(),
-  DEFAULT_SAVE_PATH: path.join(os.homedir(), 'joystream', 'download', path.sep),
-  AUTO_UPDATE_BASE_URL: 'https://download.joystream.co:7070/update/'
-}
+export const POST_TORRENT_UPDATES_INTERVAL = 3000
+export const AUTO_UPDATE_BASE_URL ='https://download.joystream.co:7070/update/'
+
+// Not used - just keeping them here as hint to use in future
+//export const DEFAULT_TORRENT_FILE_SOURCE_LOCATION = os.homedir()
+//export const DEFAULT_SAVE_PATH= path.join(os.homedir(), 'joystream', 'download', path.sep)
