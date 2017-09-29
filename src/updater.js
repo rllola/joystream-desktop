6fbeadba0a87b27327efb2b2bd3b1a371b69d580
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2017-09-29 11:57:34 +0300

    autoupdater - check for available update independantly of builtin electron autoUpdater

--- src/updater.js
+++ src/updater.js
@@ -1,54 +1,79 @@
 const electron = require('electron')
+const request = require('request')
+
 const APP_VERSION = require('../package.json').version
 const AUTO_UPDATE_BASE_URL = require('./constants').AUTO_UPDATE_BASE_URL
+const AUTO_UPDATE_FEED_URL = AUTO_UPDATE_BASE_URL + process.platform + '/' + APP_VERSION
 
-const AUTO_UPDATE_URL = AUTO_UPDATE_BASE_URL + process.platform + '/' + APP_VERSION
+function init (feedUrl = AUTO_UPDATE_FEED_URL) {
+  // Auto updates not available on linux
+  if (process.platform === 'linux') return
 
-function init () {
-  if (process.platform === 'linux') {
-    console.log('Auto updates not available on linux')
-  } else {
-    initDarwinWin32()
-  }
-}
-
-function initDarwinWin32 () {
-  electron.autoUpdater.on(
-    'error',
-    (err) => console.error(`Update error: ${err.message}`))
-
-  electron.autoUpdater.on(
-    'checking-for-update',
-    () => console.log('Checking for update'))
-
-  electron.autoUpdater.on(
-    'update-available',
-    () => console.log('Update available'))
-
-  electron.autoUpdater.on(
-    'update-not-available',
-    () => console.log('No update available'))
-
-  // Ask the user if update is available
-  electron.autoUpdater.on(
-    'update-downloaded',
-    (event, releaseNotes, releaseName) => {
+  checkForUpdate(feedUrl, function (err, releaseName) {
+    if (!err) {
+      // If update is available, prompt user if they want to download it or not
       electron.dialog.showMessageBox({
         type: 'question',
-        buttons: ['Update', 'Cancel'],
+        buttons: ['Download', 'Cancel'],
         defaultId: 0,
-        message: `Version ${releaseName} is available, do you want to install it now?`,
-        title: 'Update available'
+        message: 'A new version of JoyStream is available, do you want to download it now?',
+        title: `Download version ${releaseName}`
       }, response => {
-        if (response === 0) {
-          electron.autoUpdater.quitAndInstall()
-        }
+        if (response === 0) downloadUpdate(feedUrl)
       })
     }
-  )
+  })
+}
 
-  electron.autoUpdater.setFeedURL(AUTO_UPDATE_URL)
+function checkForUpdate (feedUrl, callback) {
+  request({url: feedUrl}, function (err, response, body) {
+    if (err) return callback(err)
+
+    if (response.statusCode === 200) {
+      try {
+        var updateInfo = JSON.parse(body)
+      } catch (e) {
+        return callback(new Error('failed to parse response from server'))
+      }
+      callback(null, updateInfo.name)
+    } else {
+      callback(new Error('Update Not Found'))
+    }
+  })
+}
+
+function downloadUpdate (feedUrl) {
+  // Electron autoUpdater checks for update and automatically downloads update if available
+  // it doesn't give user a choice
+  electron.autoUpdater.setFeedURL(feedUrl)
   electron.autoUpdater.checkForUpdates()
+}
+
+electron.autoUpdater.on('error', (err) => {
+  electron.dialog.showMessageBox({
+    type: 'error',
+    defaultId: 0,
+    message: `Update Error: ${err.message}`,
+    title: 'Update Error'
+  })
+})
+
+// When update has been downloaded
+electron.autoUpdater.on('update-downloaded', confirmInstallation)
+
+// Prompt user if they want to install update or not
+function confirmInstallation (event, releaseNotes, releaseName) {
+  electron.dialog.showMessageBox({
+    type: 'question',
+    buttons: ['Install', 'Cancel'],
+    defaultId: 0,
+    message: 'Latest version downloaded, do you want to install it now?',
+    title: `Update to version ${releaseName}`
+  }, response => {
+    if (response === 0) {
+      electron.autoUpdater.quitAndInstall()
+    }
+  })
 }
 
 module.exports = {
