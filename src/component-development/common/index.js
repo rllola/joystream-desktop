b5438cec9561a2a40fd2eec38fde3c37546c0e51
Bedeho Mender <bedeho.mender@gmail.com>
2017-10-04 18:12:56 +0200

    Make scenario container which can be styled

--- src/component-development/common/index.js
+++ src/component-development/common/index.js
@@ -2,18 +2,23 @@
  * Created by bedeho on 29/05/17.
  */
 import React from 'react'
+import PropTypes from 'prop-types'
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 const ScenarioContainer = (props) => {
 
+    let style = Object.assign({paddingBottom : 40}, props.style)
+
     return (
-        <div style={{paddingBottom : 40}}>
-            <Card>
-                <CardTitle title={props.title} subtitle={props.subtitle} />
-                {props.children}
-            </Card>
-        </div>
+        <Card style={style}>
+            <CardTitle title={props.title} subtitle={props.subtitle} />
+            {props.children}
+        </Card>
     )
+}
+
+ScenarioContainer.propTypes = {
+    style : PropTypes.object
 }
 
 export {ScenarioContainer}
