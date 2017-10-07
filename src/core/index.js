ff5a3525e740c4261cb7db4940ab017959f0bff6
Bedeho Mender <bedeho.mender@gmail.com>
2017-10-07 18:20:53 +0200

    New UI store for onboarding

---
+++ src/core/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by bedeho on 04/10/17.
+ */
+
+import OnboardingStore from './OnboardingStore'
+import ApplicationSettings from './ApplicationSettings'
+
+export {
+    OnboardingStore,
+    ApplicationSettings
+}
