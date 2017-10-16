0835e2d3548fafd0266182938b303e8318d60235
rllola <rllola80@gmail.com>
2017-10-16 20:58:14 +0200

    Added menu for OSX (need to be tested)

--- src/menu.js
+++ src/menu.js
@@ -24,4 +24,42 @@ const template = [
   }
 ]
 
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: 'Joystream',
+    submenu: [
+      {role: 'about'},
+      {type: 'separator'},
+      {role: 'services', submenu: []},
+      {type: 'separator'},
+      {role: 'hide'},
+      {role: 'hideothers'},
+      {role: 'unhide'},
+      {type: 'separator'},
+      {role: 'quit'}
+    ]
+  })
+
+  // Edit menu
+  template[1].submenu.push(
+    {type: 'separator'},
+    {
+      label: 'Speech',
+      submenu: [
+        {role: 'startspeaking'},
+        {role: 'stopspeaking'}
+      ]
+    }
+  )
+
+  // Window menu
+  template[3].submenu = [
+    {role: 'close'},
+    {role: 'minimize'},
+    {role: 'zoom'},
+    {type: 'separator'},
+    {role: 'front'}
+  ]
+}
+
 export { template }
