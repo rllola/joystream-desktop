f90f56f7a117c4b963ead4bef9f59c5771be7ea8
rllola <rllola80@gmail.com>
2017-11-23 05:50:18 +0100

    Linting completed scene

--- src/scenes/Completed/components/TorrentTable.js
+++ src/scenes/Completed/components/TorrentTable.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 
 import Table from '../../../components/Table'
 import TorrentRow from './TorrentRow'
-import TorrentToolbar from './TorrentToolbar'
 import TorrentContextMenu from './TorrentContextMenu'
 import ToolbarVisibilityType from '../../../utils/ToolbarVisibilityState'
 
@@ -14,126 +13,113 @@ import { contextMenuHiddenState, contextMenuVisibleState, contextMenuRect } from
 
 @observer
 class TorrentsTable extends Component {
+  constructor (props) {
+    super(props)
 
-    constructor(props) {
-        super(props)
+    // Start out with hidden context menu
+    this.state = contextMenuHiddenState()
+  }
 
-        // Start out with hidden context menu
-        this.state = contextMenuHiddenState()
+  hideContextMenu () {
+    // Schedule updating state to have: hidden context menu
+    this.setState(contextMenuHiddenState())
+  }
+
+  isContextMenuVisible () {
+    return this.state.contextMenu !== null
+  }
+
+  toolbarMoreButtonClicked (e, torrent) {
+    // If context menu is already visible, then hide
+    if (this.isContextMenuVisible()) {
+      this.hideContextMenu()
+    } else {
+      // If its not visible, we must show it, which also
+      // requires that we provides intelligent positioning, which
+      // is sensitive to the location of the toolbar w.r.t. the table,
+      // and the table dimensions
+
+      // SOMETHING MORE ROBUST HERE, THIS TECHQNIUE WILL BREAK IF ANYTHING CHANGES
+      // WE NEED TO FIND DOM NODES USING COMPONENT PONTERS OR SOMETHING
+
+      // DOM Node rectangle for more button w.r.t. view port
+      var moreButtonRect = e.target.getBoundingClientRect()
+
+      // Find DOM Node for table within which context menu should be positioned
+      var tableContentRect = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.getBoundingClientRect()
+
+      // Compute rect for context menu
+      var rect = contextMenuRect(moreButtonRect, tableContentRect)
+
+      // Update state to render new context menu visiblity
+      this.setState(contextMenuVisibleState(rect.top, rect.left, torrent))
+    }
+  }
+
+  render () {
+    return (
+      <Table column_titles={['', 'UPLOADING', 'SIZE', 'BUYERS']}>
+        { this.getRenderedContextMenu() }
+        { this.getRenderedTorrentRows() }
+      </Table>
+    )
+  }
+
+  getRenderedContextMenu () {
+    return (
+      this.isContextMenuVisible()
+      ? <AbsolutePositionChildren left={this.state.contextMenu.left}
+        top={this.state.contextMenu.top}
+        zIndex={1}>
+        <TorrentContextMenu onOutsideContextMenuClicked={() => { this.hideContextMenu() }}
+          onRemoveClicked={() => { this.state.contextMenu.torrent.remove(); this.hideContextMenu() }}
+          onRemoveAndDeleteDataClicked={() => { this.state.contextMenu.torrent.removeAndDeleteData(); this.hideContextMenu() }}
+          numberOfBuyers={this.state.contextMenu.torrent.numberOfBuyers}
+          numberOfObservers={this.state.contextMenu.torrent.numberOfObservers}
+          numberOfNormalPeers={this.state.contextMenu.torrent.numberOfNormalPeers} />
+      </AbsolutePositionChildren>
+      : null
+    )
+  }
+
+  getRenderedTorrentRows () {
+    return (this.props.torrents.map((t, index) => { return this.getRenderedTorrentRow(t, index % 2 === 0) }))
+  }
+
+  getRenderedTorrentRow (t, isEven) {
+    var toolbarProps = {
+      onOpenFolderClicked: () => { t.openFolder() },
+      onMoreClicked: (e) => { this.toolbarMoreButtonClicked(e, t) }
     }
 
-    hideContextMenu() {
+    let backgroundColor = isEven ? 'hsla(0, 0%, 93%, 1)' : 'white'
 
-        // Schedule updating state to have: hidden context menu
-        this.setState(contextMenuHiddenState())
+    return (
+      <TorrentRow key={t.infoHash}
+        torrent={t}
+        toolbarVisibilityStatus={this.getToolbarVisibilityTypeForTorrent(t)}
+        toolbarProps={toolbarProps}
+        store={this.props.store}
+        backgroundColor={backgroundColor} />
+    )
+  }
+
+  getToolbarVisibilityTypeForTorrent (t) {
+    if (this.isContextMenuVisible()) {
+      if (t === this.state.contextMenu.torrent) {
+        return ToolbarVisibilityType.Visible
+      } else {
+        return ToolbarVisibilityType.Hidden
+      }
+    } else {
+      return ToolbarVisibilityType.OnHover
     }
-
-    isContextMenuVisible() {
-        return this.state.contextMenu !== null
-    }
-
-    toolbarMoreButtonClicked(e, torrent) {
-
-        // If context menu is already visible, then hide
-        if(this.isContextMenuVisible()) {
-            this.hideContextMenu()
-        } else {
-
-            // If its not visible, we must show it, which also
-            // requires that we provides intelligent positioning, which
-            // is sensitive to the location of the toolbar w.r.t. the table,
-            // and the table dimensions
-
-
-            /// SOMETHING MORE ROBUST HERE, THIS TECHQNIUE WILL BREAK IF ANYTHING CHANGES
-            /// WE NEED TO FIND DOM NODES USING COMPONENT PONTERS OR SOMETHING
-
-            // DOM Node rectangle for more button w.r.t. view port
-            var moreButtonRect = e.target.getBoundingClientRect()
-
-            // Find DOM Node for table within which context menu should be positioned
-            var tableContentRect = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.getBoundingClientRect()
-
-            // Compute rect for context menu
-            var rect = contextMenuRect(moreButtonRect, tableContentRect)
-
-            // Update state to render new context menu visiblity
-            this.setState(contextMenuVisibleState(rect.top, rect.left, torrent))
-        }
-    }
-
-    render() {
-
-        return (
-            <Table column_titles={["", "UPLOADING", "SIZE", "BUYERS"]}>
-                { this.getRenderedContextMenu() }
-                { this.getRenderedTorrentRows() }
-            </Table>
-        )
-    }
-
-    getRenderedContextMenu() {
-
-        return (
-            this.isContextMenuVisible()
-            ?
-            <AbsolutePositionChildren left={this.state.contextMenu.left}
-                                      top={this.state.contextMenu.top}
-                                      zIndex={1}>
-                <TorrentContextMenu onOutsideContextMenuClicked = {() => { this.hideContextMenu()}}
-                                    onRemoveClicked = {() => {this.state.contextMenu.torrent.remove(); this.hideContextMenu()}}
-                                    onRemoveAndDeleteDataClicked = {() => {this.state.contextMenu.torrent.removeAndDeleteData(); this.hideContextMenu()}}
-                                    numberOfBuyers = {this.state.contextMenu.torrent.numberOfBuyers}
-                                    numberOfObservers = {this.state.contextMenu.torrent.numberOfObservers}
-                                    numberOfNormalPeers = {this.state.contextMenu.torrent.numberOfNormalPeers}/>
-            </AbsolutePositionChildren>
-            :
-            null
-        )
-    }
-
-    getRenderedTorrentRows() {
-
-        return (this.props.torrents.map((t, index) => { return this.getRenderedTorrentRow(t, index % 2 === 0) }))
-    }
-
-    getRenderedTorrentRow(t, isEven) {
-
-        var toolbarProps = {
-            onOpenFolderClicked : () => { t.openFolder() },
-            onMoreClicked : (e) => { this.toolbarMoreButtonClicked(e, t) }
-        }
-
-        let backgroundColor = isEven ? 'hsla(0, 0%, 93%, 1)' : 'white'
-
-        return (
-            <TorrentRow key={t.infoHash}
-                        torrent={t}
-                        toolbarVisibilityStatus = {this.getToolbarVisibilityTypeForTorrent(t)}
-                        toolbarProps={toolbarProps}
-                        store={this.props.store}
-                        backgroundColor={backgroundColor}
-            />
-        )
-    }
-
-    getToolbarVisibilityTypeForTorrent(t) {
-
-        if(this.isContextMenuVisible()) {
-
-            if(t == this.state.contextMenu.torrent)
-                return ToolbarVisibilityType.Visible
-            else
-                return ToolbarVisibilityType.Hidden
-
-        } else
-            return ToolbarVisibilityType.OnHover
-
-    }
+  }
 }
 
 TorrentsTable.propTypes = {
-    torrents : PropTypes.array.isRequired // Further refine this to require particular object (shapes) in array?
+  store: PropTypes.object.isRequired,
+  torrents: PropTypes.array.isRequired // Further refine this to require particular object (shapes) in array?
 }
 
 export default TorrentsTable
--- src/scenes/Completed/components/TorrentToolbar.js
+++ src/scenes/Completed/components/TorrentToolbar.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import isRequiredIf from 'react-proptype-conditional-require'
 
 import Toolbar, {
     OpenFolderSection,
@@ -10,20 +9,20 @@ import Toolbar, {
     StartUploadingSection} from '../../../components/Toolbar'
 
 const TorrentToolbar = (props) => {
-    return (
-        <Toolbar>
-            <PlaySection torrent={props.torrent} />
-            <StartUploadingSection torrent={props.torrent}/>
-            <RemoveSection torrent={props.torrent} store={props.store} />
-            <RemoveAndDeleteSection torrent={props.torrent} store={props.store} />
-            <OpenFolderSection torrent={props.torrent} />
-        </Toolbar>
-    )
+  return (
+    <Toolbar>
+      <PlaySection torrent={props.torrent} />
+      <StartUploadingSection torrent={props.torrent} />
+      <RemoveSection torrent={props.torrent} store={props.store} />
+      <RemoveAndDeleteSection torrent={props.torrent} store={props.store} />
+      <OpenFolderSection torrent={props.torrent} />
+    </Toolbar>
+  )
 }
 
 TorrentToolbar.propTypes = {
-    torrent : PropTypes.object.isRequired,
-    store : PropTypes.object.isRequired
+  torrent: PropTypes.object.isRequired,
+  store: PropTypes.object.isRequired
 }
 
 export default TorrentToolbar
