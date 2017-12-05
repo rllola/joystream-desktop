d5a16baadc61b7ee9c729329faf49c986f08b076
rllola <rllola80@gmail.com>
2017-12-05 07:49:25 +0100

    Refactor components table for seeding scene

--- src/scenes/Seeding/TorrentTable.js
+++ src/scenes/Seeding/TorrentTable.js
@@ -5,14 +5,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import Table, {Hint} from '../../components/Table'
-import TorrentRow from './TorrentRow'
-import TorrentContextMenu from './TorrentContextMenu'
-import ToolbarVisibilityType from '../../utils/ToolbarVisibilityState'
-import { AbsolutePositionChildren } from '../../common'
+import Table, { Hint } from '../../components/Table'
+import TorrentRows from './components/TorrentRows'
 import Dropzone from 'react-dropzone'
-
-import { contextMenuHiddenState, contextMenuVisibleState, contextMenuRect } from '../../utils/ContextMenuHelper'
 
 class TorrentsTable extends Component {
 
@@ -25,49 +20,6 @@ class TorrentsTable extends Component {
    * }
    */
 
-  constructor (props) {
-    super(props)
-
-    // Start out with hidden context menu
-    this.state = contextMenuHiddenState()
-  }
-
-  hideContextMenu () {
-    // Schedule updating state to have: hidden context menu
-    this.setState(contextMenuHiddenState())
-  }
-
-  isContextMenuVisible () {
-    return this.state.contextMenu !== null
-  }
-
-  toolbarMoreButtonClicked (e, torrent) {
-    // If context menu is already visible, then hide
-    if (this.isContextMenuVisible()) {
-      this.hideContextMenu()
-    } else {
-      // If its not visible, we must show it, which also
-      // requires that we provides intelligent positioning, which
-      // is sensitive to the location of the toolbar w.r.t. the table,
-      // and the table dimensions
-
-      // SOMETHING MORE ROBUST HERE, THIS TECHQNIUE WILL BREAK IF ANYTHING CHANGES
-      // WE NEED TO FIND DOM NODES USING COMPONENT PONTERS OR SOMETHING
-
-      // DOM Node rectangle for more button w.r.t. view port
-      var moreButtonRect = e.target.getBoundingClientRect()
-
-      // Find DOM Node for table within which context menu should be positioned
-      var tableContentRect = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.getBoundingClientRect()
-
-      // Compute rect for context menu
-      var rect = contextMenuRect(moreButtonRect, tableContentRect)
-
-      // Update state to render new context menu visiblity
-      this.setState(contextMenuVisibleState(rect.top, rect.left, torrent))
-    }
-  }
-
   render () {
     var dropZoneStyle = {
       display: 'flex',
@@ -79,71 +31,12 @@ class TorrentsTable extends Component {
     return (
       <Dropzone disableClick style={dropZoneStyle} onDrop={(files) => { this.props.store.startTorrentUploadFlowWithTorrentFile(files) }}>
         <Table column_titles={['', 'STATE', 'SPEED', 'PRICE', 'REVENUE', 'BUYERS']}>
-          { this.getRenderedContextMenu() }
-          { this.getRenderedTorrentRows() }
+          {this.props.torrents.length === 0
+            ? <Hint title='Drop torrent file here to start uploading' key={0} />
+            : <TorrentRows torrents={this.props.torrents} store={this.props.store} />}
         </Table>
       </Dropzone>
     )
-  }
-
-  getRenderedContextMenu () {
-    return (
-      this.isContextMenuVisible()
-    ? <AbsolutePositionChildren left={this.state.contextMenu.left}
-      top={this.state.contextMenu.top}
-      zIndex={1}>
-      <TorrentContextMenu onOutsideContextMenuClicked={() => { this.hideContextMenu() }}
-        paused={this.state.contextMenu.torrent.paused}
-        onChangePauseStatus={() => { this.state.contextMenu.torrent.changePauseStatus(); this.hideContextMenu() }}
-        changePriceEnabled={this.state.contextMenu.torrent.canChangePrice}
-        onChangePriceClicked={() => { this.state.contextMenu.torrent.showChangePriceDialog(); this.hideContextMenu() }}
-        onRemoveClicked={() => { this.state.contextMenu.torrent.remove(); this.hideContextMenu() }}
-        onRemoveAndDeleteDataClicked={() => { this.state.contextMenu.torrent.removeAndDeleteData(); this.hideContextMenu() }}
-        numberOfBuyers={this.state.contextMenu.torrent.numberOfBuyers}
-        numberOfSellers={this.state.contextMenu.torrent.numberOfSellers}
-        numberOfObservers={this.state.contextMenu.torrent.numberOfObservers}
-        numberOfNormalPeers={this.state.contextMenu.torrent.numberOfNormalPeers} />
-    </AbsolutePositionChildren>
-    : null
-    )
-  }
-
-  getRenderedTorrentRows () {
-    return (
-      this.props.torrents.length === 0
-      ? <Hint title='Drop torrent file here to start uploading' key={0} />
-      : this.props.torrents.map((t, index) => { return this.getRenderedTorrentRow(t, index % 2 === 0) })
-    )
-  }
-
-  getRenderedTorrentRow (t, isEven) {
-    var toolbarProps = {
-      onOpenFolderClicked: () => { t.openFolder() },
-      onMoreClicked: (e) => { this.toolbarMoreButtonClicked(e, t) }
-    }
-
-    let backgroundColor = isEven ? 'hsla(0, 0%, 93%, 1)' : 'white'
-
-    return (
-      <TorrentRow key={t.infoHash}
-        torrent={t}
-        toolbarVisibilityStatus={this.getToolbarVisibilityTypeForTorrent(t)}
-        toolbarProps={toolbarProps}
-        store={this.props.store}
-        backgroundColor={backgroundColor} />
-    )
-  }
-
-  getToolbarVisibilityTypeForTorrent (t) {
-    if (this.isContextMenuVisible()) {
-      if (t === this.state.contextMenu.torrent) {
-        return ToolbarVisibilityType.Visible
-      } else {
-        return ToolbarVisibilityType.Hidden
-      }
-    } else {
-      return ToolbarVisibilityType.OnHover
-    }
   }
 }
 
